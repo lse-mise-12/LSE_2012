7d3fef3982ad936621e3ab6e75471487705d4a08
Carlos Benito <carlosbs@usal.es>
2012-04-13 19:12:26 +0200

    Temperature driver, real temperature not tested

--- tempSensor/driver/temperature_driver.c
+++ tempSensor/driver/temperature_driver.c
@@ -43,7 +43,7 @@ struct file_operations temp_fops = {
 };
 
 /* Global variables */
-int temp_major = 73;
+int temp_major = 74;
 
 
 int temp_init (void){
@@ -51,12 +51,12 @@ int temp_init (void){
 
 	//Register type of device: character (or block, net)
 
-	result = register_chrdev(temp_major, "temperature2", &temp_fops);
+	result = register_chrdev(temp_major, "temperature", &temp_fops);
 	if (result < 0){
-		printk("<1>Fail major number = %d\n", temp_major);
+		printk("<1>Fail major number for temp_Driver = %d\n", temp_major);
 		return result;
 	}else{
-		printk("<1>Good major number = %d\n", temp_major);		
+		printk("<1>Good major number for temp_driver = %d\n", temp_major);		
 	}
 
 	printk("<1>Temperature driver initialized..\n");
@@ -65,10 +65,10 @@ int temp_init (void){
 
 void temp_exit (void) {
 	//free major number
-	unregister_chrdev(temp_major, "spi_if");
+	unregister_chrdev(temp_major, "temperature");
 	//Unlock access to register
 	//release_region(REG_ADDR_PEDR, REG_NUMB);
-	printk("<1>Bye bye!\n");
+	printk("<1>Temperature driver exit succesfully\n");
 }
 
 char check_reg(int reg_add, int reg_value)
@@ -83,24 +83,24 @@ char check_reg(int reg_add, int reg_value)
 /* Open device (as file) */
 int temp_open(struct inode *inode, struct file *filp) {
 	
-//	MOD_INC_USE_COUNT;
+	MOD_INC_USE_COUNT;
 	printk("<1>Initializing temp driver\n");
 	return 0;
 }
 
 /* Close device (as file) */
 int temp_release(struct inode *inode, struct file *filep) {
-	int sspcr1_val_clear = 0x00000000;
-	outl(sspcr1_val_clear,SSPCR1); //Clear SSE (disable SPI)
+//	int sspcr1_val_clear = 0x00000000;
+//	outl(sspcr1_val_clear,SSPCR1); //Clear SSE (disable SPI)
 	MOD_DEC_USE_COUNT;
-	printk("<1>Closing\n");
+	printk("<1>Closing temp driver\n");
 	return 0;
 }
 
 /* Read from device (read from register) */
 ssize_t temp_read(struct file *filep, char *buf, size_t count, loff_t *f_pos) {
 	unsigned long val;
-	double temp;
+	float temp;
 	volatile unsigned char *chip_select_page, *ssp_page;
 	unsigned char isNegative = FALSE;
 
@@ -116,7 +116,6 @@ ssize_t temp_read(struct file *filep, char *buf, size_t count, loff_t *f_pos) {
 	be used to configure and enable the SPI bus
 	http://www-s.ti.com/sc/ds/tmp124.pdf
 	*/
-	printk("<1> traza 1\n");
 	
 	ssp_page = (unsigned char)SSP_PAGE;
 	chip_select_page = (unsigned char)CHIP_SELECT_PAGE;
@@ -125,16 +124,13 @@ ssize_t temp_read(struct file *filep, char *buf, size_t count, loff_t *f_pos) {
 //	printk("");
 //	outw(0x10, SSP_PAGE + SSPCR1); No funciona si se hace la suma ssp_page+sspcr1, ponemos el valor final en los in/out
 	outw(0x10, 0x808A0004);
-	printk("<1> traza1.1\n");
 	/* 2.)   Write other SSP config registers(SSPCR0 & SSPCPSR)*/
 	//POKE32( (unsigned long)ssp_page, 0x0F ); 
 	//POKE32( (unsigned long)(ssp_page + SSPCPSR), 0xFE ); 
 	outw(0x0F, SSP_PAGE);
-	printk("<1> traza1.2\n");
 //	outw(0xFE, SSP_PAGE + SSPCPSR); 
 	outw(0xFE, 0x808A0010);
 	
-	printk("<1> traza 2\n");
 
 
 
@@ -160,10 +156,6 @@ ssize_t temp_read(struct file *filep, char *buf, size_t count, loff_t *f_pos) {
 	//usleep(1000);
 	// while hasta que el flag de spi este correcto
 
-	printk("<1> traza 3\n");
-
-	
-
 	//disable chip select
 	//POKE32( (unsigned long)(chip_select_page + CHIP_SELECT_DDR), 0x00 );
 	outw(0x00, 0x808A0034);
@@ -171,7 +163,8 @@ ssize_t temp_read(struct file *filep, char *buf, size_t count, loff_t *f_pos) {
 	//read the temp
 	//val = PEEK32( (unsigned long)(ssp_page + SSP_DATA) );
 	val = inw(0x808A0008);
-	printk("<1> traza 4\n");  //It should work, almost until this point
+	val = 0x05;
+	printk("<1> traza 4: val=%lu\n",val);  //It should work, almost until this point
 	
 	//Lets check if the value is negative
 	if( val <= 0xFFFF && val >= 0xE487 )
@@ -180,29 +173,43 @@ ssize_t temp_read(struct file *filep, char *buf, size_t count, loff_t *f_pos) {
 		val = ~val + 1;
 		isNegative = TRUE;
 	}else if( val <= 0x4B08 && val >= 0xE486 ){
-		printk("<1>FAIL, invalid register value(out of range)...\n");
+		printk("<1>FAIL, invalid register value(out of range)...[Temp Driver]\n");
 		return 2;
 	}
 
-	printk("<1> traza 5\n");
 
 	if( val >= 0x3E88 && val <= 0x4B07)
 	{
-		temp = val / 128.046;
+		printk("<1> traza 5.1\n");
+		temp = (float)(val) / 128.046;
+		printk("<1> traza 5.2\n");
 
 	}else if( val >= 0xC88 && val <= 0x3E87 ){
-		temp = val / 128.056;
+		printk("<1> traza 5.3\n");
+		temp = (float)(val) / 128.056;
+		printk("<1> traza 5.4\n");
 	}else if( val >= 0x10 && val <= 0xC87 ){
-		temp = val / 128.28;
+		printk("<1> traza 5.5\n");
+		temp = (float)(val) / 128.28;
+		printk("<1> traza 5.6\n");
 	}else{
-		temp = val / 240;
+		printk("<1> traza 5.7\n");
+		temp = (float)(val) / 240;
+		printk("<1> traza 5.8\n");
 	}
-	//snprintf(buf, 7, "%f", temp);
+	
+	int a;
+	a = temp * 1000;
+	sprintf(buf, "%d", a);
+//	sprintf(buf, "prueba snprintf\n");
+//	*buf = 42;
+	//return 1;
+	
 	
 	//buf[0] = "1";
-	printk("<1> temp = %f\n",temp);
+//	printk("<1> temp = %f\n",temp);
 
-   	return 1;
+   	return 7; //strlen(buf);
 }
 	/* Write to device (write to register) */
 ssize_t temp_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos) {
